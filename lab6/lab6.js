5fe20e64c2050ff16161c82f94180aa104de6c22
AbubakarSalimSectionD <n01709661@humber.ca>
2024-10-11 15:00:56 -0400

    Create lab6.js

---
+++ lab6/lab6.js
@@ -0,0 +1,28 @@
+let myNumber = 10;
+let myString = "Hello, JavaScript!";
+
+console.log(myNumber);
+console.log(myString);
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i); 
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(3 * i); 
+}
+
+function addNumbers(num1, num2) {
+    return num1 + num2; 
+}
+
+let sum = addNumbers(5, 10); 
+console.log("Sum:", sum);
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2; 
+}
+
+let product = multiplyNumbers(4, 6); 
+console.log("Product:", product);
+
